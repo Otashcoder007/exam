61787d0bd072374c840df0c0fdb286fad63910c2
Otashcoder007 <botirovotabek392@gmail.com>
2026-01-28 15:35:21 +0500

    Delete src directory

--- src/core/data-source.ts
+++
@@ -1,8 +0,0 @@
-import {DataSource} from "typeorm";
-
-export const AppDataSource = new DataSource({
-    type: "postgres",
-    url: process.env.DB_URL,
-    entities: ["./dist/**/*.entity.js"],
-    synchronize: true,
-});
