2a081d99e01bd5148376ad04229c20353a025aba
Otashcoder007 <botirovotabek392@gmail.com>
2026-01-29 20:08:28 +0500

    Delete Exam_dars412 directory

--- Exam_dars412/src/features/library/controllers/book/book-review.controller.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import {AppDataSource} from "../../../../core/data-source.js";
-import {BookReview} from "../../entities/book/book-review.entity.js";
-import {ReviewDto} from "../../dtos/review.dto.js";
-import {validateBody} from "../../../../core/middlewares/validate.middleware.js";
-import {authenticate} from "../../../../core/middlewares/auth.middleware.js";
-import {ErrorException} from "../../../../core/exception/error.exception.js";
-
-export const bookReviewRouter = Router();
--- Exam_dars412/src/features/library/controllers/course/course-review.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { AppDataSource } from "../../../../core/data-source.js";
-import { CourseReview } from "../../entities/course/course-review.entity.js";
-import { ReviewDto } from "../../dtos/review.dto.js";
-import { validateBody } from "../../../../core/middlewares/validate.middleware.js";
-import { authenticate } from "../../../../core/middlewares/auth.middleware.js";
-import { ErrorException } from "../../../../core/exception/error.exception.js";
-
-export const courseReviewRouter = Router();
